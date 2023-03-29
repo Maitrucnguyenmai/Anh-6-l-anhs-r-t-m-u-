5a48cb19255dc8cb867d5b4019d130c640c324c7
Maitrucnguyenmai <129303850+Maitrucnguyenmai@users.noreply.github.com>
2023-03-29 22:21:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    titleWeb: "Ơ học Anh 06 là được tỏ tình thật à",
+    introTitle: '8 quàng tữ đẹp troai zcllll',
+    introDesc: `Biết bàn tay muốn chạm vào ai rùi
+    Biết trong đầu này chỉ có ai đó thui
+    Biết đôi má của em đã ửng hồng rồi
+    Biết trái tim này lỡ mất vài nhịp rùi`,
+    btnIntro: 'XOXO',
+    title: 'Trúng hay hụt chắc cupid không hụt đâu',
+    desc: 'Và ta cũng lỡ thích nhau hơi nhiều quá rùi nà ',
+    btnYes: 'Chết mê rồiii',
+    btnNo: 'Chưa chắc đã giòn đâu',
+    question: 'Kể tên 8 người đẹp trai nhất hôm nay',
+    btnReply: 'Submit',
+    reply: 'Chính Duy Huy Kiên Phúc Quyết Kthaor Tùng',
+    mess: 'Chuẩn nunnn',
+    messDesc: 'Cùng viết lên câu chuyện đến khi chỉ còn một lời để nói',
+    btnAccept: 'Yes I love you babes ^^',
+    messLink: 'https://www.messenger.com/t/5587528154644210' 
